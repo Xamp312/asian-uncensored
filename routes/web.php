cb4477ea207a73ee6984ef01262589dd1ca8c2a2
Haadi Raja <haadiraja@gmail.com>
2021-08-22 16:59:37 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,58 +17,46 @@ use App\Models\User;
 |
  */
 Route::get('/', [HomeController::class, 'landingPage'])->name('landingPage');
-
 Route::get('/videos/latest', [VideoController::class, 'latestVideos'])->name('latestVideos');
 Route::get('/videos/mostviews', [VideoController::class, 'mostViewsVideos'])->name('mostViewsVideos');
 Route::get('/videos/mostlikes', [VideoController::class, 'mostLikesVideos'])->name('mostLikesVideos');
-
 Route::get('/video/upload', [VideoController::class, 'videoUploadPage'])->name('videoUploadPage');
 Route::get('/video/{slug}', [VideoController::class, 'videoPage'])->name('videoPage');
-
 Route::post('/video/react', [VideoController::class, 'videoReact'])->name('videoReact');
 Route::post('/video/rate', [VideoController::class, 'videoRate'])->name('videoRate');
-
 Route::post('/video/upload', [VideoController::class, 'upload'])->name('videoUpload');
-
+Route::get('/community', [CommunityController::class, 'communityPage'])->name('communityPage');
 Route::get('/profile/{slug}','App\Http\Controllers\VideoController@profile' )->name('profile');
 
-Route::get('/login', function () {
-    return view('pages.login');
-});
-
-Route::get('/register', function () {
-    return view('pages.register');
-});
+//Authentication Routes
+Route::get('/login', function () { return view('pages.login');});
+Route::get('/register', function () {return view('pages.register');});
+//Authentication Routes End
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('auth');
+Route::group(['middleware' => ['auth']], function () { 
 
+Route::get('/settings', function () { return view('pages.settings'); });
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 //Admin Routes
 Route::get('/category/new', 'App\Http\Controllers\AdminController@newCat')->name('newCat');
 Route::get('/category/edit/{id}', 'App\Http\Controllers\AdminController@editCat')->name('editCat');
 Route::post('/category/create', 'App\Http\Controllers\AdminController@createCat')->name('createCat');
 Route::post('/category/update', 'App\Http\Controllers\AdminController@updateCat')->name('updateCat');
-
-
-
 Route::get('/users/list', 'App\Http\Controllers\AdminController@users')->name('users');
-
 Route::get('user/delete/{slug}', 'App\Http\Controllers\AdminController@deleteUser')->name('deleteUser');
 Route::get('user/makeAdmin/{slug}', 'App\Http\Controllers\AdminController@makeAdmin')->name('makeAdmin');
 Route::get('user/removeAdmin/{slug}', 'App\Http\Controllers\AdminController@removeAdmin')->name('removeAdmin');
-
-
 //Admin Routes End
 
-Route::get('/settings', function () {
-    return view('pages.settings');
 });
 
 
 
 
-Route::get('/community', [CommunityController::class, 'communityPage'])->name('communityPage');
+
+
 
 
